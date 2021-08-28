96d062c7b22a597945d5fc0393233f9f3289d8cd
joaquin <jecorrea@protonmail.com>
2021-08-28 19:31:27 -0400

    links partially working

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ class NavBar extends Component {
   render() {
     return (
       <div className="nav-bar">
-        <NavLink to="/"><h1>Joaquin Correa</h1></NavLink>
+        <NavLink to="/" className="inactive" activeClassName="active"><h1>Joaquin Correa</h1></NavLink>
         <h4>Full stack software engineer specialized on front end development and with a passion to help others.</h4> <NavLink to="/">Projects</NavLink> <NavLink to="/about">About</NavLink>
       </div>
     );
