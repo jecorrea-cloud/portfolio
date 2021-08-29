cb6dd148d9f7bbd7c7306eaf5c64d7b673a181e0
joaquin <jecorrea@protonmail.com>
2021-08-29 19:41:37 -0400

    edited css

--- src/components/About.js
+++ src/components/About.js
@@ -14,10 +14,12 @@ export default class About extends Component {
                     <img alt="pic" src={hidden}/>
                 </div>
 
-                <p> Hey there! My name is Joaquin Correa and I am a full stack software engineer, specialized on front-end 
-                development. Recently, I graduated from the Flatiron School and now I am willing make an entry into the IT world. 
-                Currently I am residing on the Greater New York area, but I am always open for moving out of state or work remotely. In my recent years I discovered we are in a world where we always need each other whether directly or indirectly. This is why I have interests in 
-                applying what I learn and constantly get more knowledge to help others to the best of my extensions. Have to say I like to see myself as a teamplayer. Besides learning about my career, I have an interest in playing the ukulele, learning foreign languages, and enjoy metal and hard rock concerts. Feel free to reach out!</p>
+                <p>
+                    Hey! My name is Joaquin Correa and I am a full stack software engineer, specialized on front-end 
+                development. Recently, I graduated from the Flatiron School and now I am actively looking to make an entry into the IT world. 
+                Currently I reside on the Greater New York area, but I am always open out of state offer. In my recent years I discovered we are in a world where we always need each other whether directly or indirectly. This is why I have interests in 
+                applying what I learn and constantly gain more knowledge to help others to the best of my extensions. I like to see myself as a teamplayer. Besides learning about my career, I have an interest in playing the ukulele, learning foreign languages, and enjoy metal and hard rock concerts. Feel free to reach out!
+                </p>
                 <br/>
                 <br/>
                 <div className="social-links">
