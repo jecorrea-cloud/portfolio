929008abe5edbd5067004edd8ede20613cba9dc0
joaquin <jecorrea@protonmail.com>
2021-08-30 13:30:26 -0400

    partially added css

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react'
 export default class Projects extends Component {
     render() {
         return (
-            <div>
+            <div className='site-content'>
                 Projects go here:
                 <br></br>
+                <br/>
+                <br/>
                 <a href="https://jecorrea-cloud.github.io/jecorrea-pokedex/" rel="opener noreferrer" target="_blank" >PokeDéxter</a>
             </div>
         )
