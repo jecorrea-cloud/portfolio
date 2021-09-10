47183ef9cd61403801894b66caeeb502f3ec0b01
joaquin <jecorrea@protonmail.com>
2021-09-10 10:23:16 -0400

    cards partially working

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,11 +4,22 @@ export default class Projects extends Component {
     render() {
         return (
             <div className='site-content'>
-                Projects go here:
-                <br></br>
                 <br/>
-                <br/>
-                <a href="https://jecorrea-cloud.github.io/jecorrea-pokedex/" rel="opener noreferrer" target="_blank" >PokeDéxter</a>
+                <div class="card">
+                    <div class="effect-img">
+                        <img src="https://i.ibb.co/tckNt0h/Poke-Dexter-Preview.png" alt="poke"/>
+                    </div>
+                    <div class="effect-text">
+                        <h2>PokeDéxter</h2>
+                        <p>A vanilla JS searcher for Pokémon stats, that uses the Pokémon API</p>
+                        <a href="https://jecorrea-cloud.github.io/jecorrea-pokedex/" rel="opener noreferrer" target="_blank" >Site</a>
+                        
+                        <a href="https://jecorrea-cloud.github.io/jecorrea-pokedex/" rel="opener noreferrer" target="_blank" >Repo</a>
+                    </div>
+                </div>
+
+
+
             </div>
         )
     }
