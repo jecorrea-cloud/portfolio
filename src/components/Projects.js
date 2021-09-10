e8ada54f8b456422c28b3b2b997c96f176189a5d
joaquin <jecorrea@protonmail.com>
2021-09-10 10:52:43 -0400

    edited card css

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,17 +4,16 @@ export default class Projects extends Component {
     render() {
         return (
             <div className='site-content'>
-                <br/>
                 <div class="card">
                     <div class="effect-img">
                         <img src="https://i.ibb.co/tckNt0h/Poke-Dexter-Preview.png" alt="poke"/>
                     </div>
                     <div class="effect-text">
                         <h2>PokeDéxter</h2>
-                        <p>A vanilla JS searcher for Pokémon stats, that uses the Pokémon API</p>
+                        <p>A JS searcher for Pokémon stats that uses the Pokémon API</p>
                         <a href="https://jecorrea-cloud.github.io/jecorrea-pokedex/" rel="opener noreferrer" target="_blank" >Site</a>
-                        
-                        <a href="https://jecorrea-cloud.github.io/jecorrea-pokedex/" rel="opener noreferrer" target="_blank" >Repo</a>
+                        <br/>
+                        <a href="https://github.com/jecorrea-cloud/jecorrea-pokedex/" rel="opener noreferrer" target="_blank" >Repo</a>
                     </div>
                 </div>
 
