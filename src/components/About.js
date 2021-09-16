e5e092a60632ec151ea6caf248286b8549136aa5
joaquin <jecorrea@protonmail.com>
2021-09-16 18:54:54 -0400

    repo links added

--- src/components/About.js
+++ src/components/About.js
@@ -16,7 +16,7 @@ export default class About extends Component {
 
                 <p>
                     My name is Joaquin Correa. I am a full stack software engineer, specialized on front-end 
-                development and recently got my certificate from the Flatiron School. Some of the technologies I work with are:
+                development with a certificate from the Flatiron School. Some of the technologies I work with are:
                 <ul>
                     <li>Git</li>
                     <li>HTML</li>
@@ -27,7 +27,8 @@ export default class About extends Component {
                     <li>Ruby / Ruby on Rails</li>
                 </ul>
                 Currently I reside on the Greater New York metro area. In my recent years I discovered we are in a reality where we always need each other's help whether directly or indirectly. This is why I have an interest in 
-                applying what I learn and constantly gain more knowledge to help others to the best of my extensions. I like to see myself as a team player. Besides learning about my career, I have an interest in playing the ukulele, learning foreign languages, and enjoying metal and hard rock concerts. Feel free to reach out!
+                applying what I learn and constantly gain more knowledge to help others to the best of my extensions. I like to see myself as a team player. 
+                Besides learning about my career, I have an interest in volunteering for social causes and local events, playing the ukulele, learning foreign languages, and enjoying metal and hard rock concerts. Feel free to reach out!
                 </p>
                 <br/>
                 <br/>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,9 +11,9 @@ export default class Projects extends Component {
                     <div class="effect-text">
                         <h2>PokeDéxter</h2>
                         <p>A JS searcher for Pokémon stats that uses the Pokémon API</p>
-                        <a href="https://jecorrea-cloud.github.io/jecorrea-pokedex/" rel="opener noreferrer" target="_blank" >Site</a>
+                        <a class="effect-link" href="https://jecorrea-cloud.github.io/jecorrea-pokedex/" rel="opener noreferrer" target="_blank" >Site</a>
                         <br/>
-                        <a href="https://github.com/jecorrea-cloud/jecorrea-pokedex/" rel="opener noreferrer" target="_blank" >Repo</a>
+                        <a class="effect-link" href="https://github.com/jecorrea-cloud/jecorrea-pokedex/" rel="opener noreferrer" target="_blank" >Repo</a>
                     </div>
                 </div>
 
@@ -24,9 +24,9 @@ export default class Projects extends Component {
                     <div class="effect-text">
                         <h2>Alive-Journal</h2>
                         <p>A to-do list made with React.js</p>
-                        <a href="https://jecorrea-cloud.github.io/jecorrea-pokedex/" rel="opener noreferrer" target="_blank" >Site</a>
+                        <a class="effect-link" href="https://jecorrea-cloud.github.io/jecorrea-pokedex/" rel="opener noreferrer" target="_blank" >Site</a>
                         <br/>
-                        <a href="https://github.com/jecorrea-cloud/jecorrea-pokedex/" rel="opener noreferrer" target="_blank" >Repo</a>
+                        <a class="effect-link" href="https://github.com/jecorrea-cloud/phase4-frontend" rel="opener noreferrer" target="_blank" >Repo</a>
                     </div>
                 </div>
 
@@ -37,9 +37,9 @@ export default class Projects extends Component {
                     <div class="effect-text">
                         <h2>Book-Cook</h2>
                         <p>Your personal recipe book made with React.js</p>
-                        <a href="https://jecorrea-cloud.github.io/jecorrea-pokedex/" rel="opener noreferrer" target="_blank" >Site</a>
+                        <a class="effect-link" href="https://jecorrea-cloud.github.io/jecorrea-pokedex/" rel="opener noreferrer" target="_blank" >Site</a>
                         <br/>
-                        <a href="https://github.com/jecorrea-cloud/jecorrea-pokedex/" rel="opener noreferrer" target="_blank" >Repo</a>
+                        <a class="effect-link" href="https://github.com/jecorrea-cloud/cookbook-frontend" rel="opener noreferrer" target="_blank" >Repo</a>
                     </div>
                 </div>
 
