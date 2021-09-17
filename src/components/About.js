e02016efe65f6d7109081b6500f68a7aba7d2d01
joaquin <jecorrea@protonmail.com>
2021-09-16 21:54:58 -0400

    about file edited

--- src/components/About.js
+++ src/components/About.js
@@ -15,7 +15,8 @@ export default class About extends Component {
                 </div>
 
                 <p>
-                    My name is Joaquin Correa. I am a full stack software engineer, specialized on front-end 
+                    
+                    &nbsp; &nbsp; My name is Joaquin Correa. I am a full stack software engineer, specialized on front-end 
                 development with a certificate from the <em>Flatiron School</em>. Some of the technologies I work with are:
                 <ul>
                     <li>Git</li>
@@ -26,7 +27,7 @@ export default class About extends Component {
                     <li>SQL</li>
                     <li>Ruby / Ruby on Rails</li>
                 </ul>
-                Currently I reside on the Greater New York metro area. In my recent years I discovered we are in a reality where we always need each other's help whether directly or indirectly. This is why I have an interest in 
+                &nbsp; &nbsp; Currently I reside on the Greater New York metro area. In my recent years I discovered we always need each other's help whether directly or indirectly. This is why I have an interest in 
                 applying what I learn and constantly gain more knowledge to help others to the best of my extensions. I like to see myself as a team player. 
                 Besides learning about my career, I have an interest in volunteering for social causes and local events, playing the ukulele, learning foreign languages, and enjoying metal and hard rock concerts. Feel free to reach out!
                 </p>
